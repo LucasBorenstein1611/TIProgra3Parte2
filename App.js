2e3b3e268e31dd5761d864c73c7d3916107f53ec
CatalinaMarcos <cmarcosaslanian@udesa.edu.ar>
2025-11-10 10:10:32 -0300

    remember me hecho

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
 import Menu from './src/components/Menu';
+import { auth } from './src/firebase/config';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login}  options={{headerShown: false}}/>
-        <Stack.Screen name="Register" component={Register} options={{headerShown: false}}/>
-        <Stack.Screen name="Menu" component={Menu} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({loggedIn: false});
+      }
+    });
+  }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          { this.state.loggedIn ? (
+            <Stack.Screen name="Menu" component={Menu} />
+          ) : (
+            <>
+              <Stack.Screen name="Login" component={Login} />
+              <Stack.Screen name="Register" component={Register} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
+
+export default App;
